1eab848de5b662a9343d2ea194b51bc32ead0c61
ncaco <ncaco97@gmail.com>
2025-11-09 16:18:47 +0900

    init

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
-
-ctx.fillStyle = 'red';
-ctx.fillRect(0, 0, 100, 100);
-
-console.log(ctx);
